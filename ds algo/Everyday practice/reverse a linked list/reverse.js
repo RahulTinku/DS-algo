793cddb218c6e2c7b06668014cea7e1c57e3e9d4
Rahul Kumar <rahulkumar271@gmail.com>
2022-02-19 19:45:45 +0530

    Add files via upload

---
+++ ds algo/Everyday practice/reverse a linked list/reverse.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let pre = null;
+  while (head) {
+    const next = head.next;
+    head.next = pre;
+    pre = head;
+    head = next;
+  }
+  return pre;
+};
+
+// alternate method
+
+var reverseList = function (head) {
+  let [prev, curr] = [null, head];
+  while (curr) {
+    [curr.next, prev, curr] = [prev, curr, curr.next];
+  }
+};
