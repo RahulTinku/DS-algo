3dc6c5db3de8267bf514bcf7235ca810a02eae15
Rahul Kumar <rahulkumar271@gmail.com>
2022-02-26 19:07:44 +0530

    Add files via upload

---
+++ ds algo/Everyday practice/best time to buy stock/best.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let result = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i + 1] > prices[i]) {
+      result += prices[i + 1] - prices[i];
+    }
+  }
+  return result;
+};
