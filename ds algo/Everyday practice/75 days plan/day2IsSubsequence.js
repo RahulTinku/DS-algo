1c29bf08954bcd2634de1103f70b47116c3b415e
Rahul Kumar <rahulkumar271@gmail.com>
2022-07-28 16:25:44 +0530

    Add files via upload

---
+++ ds algo/Everyday practice/75 days plan/day2IsSubsequence.js
@@ -0,0 +1,16 @@
+var isSubsequence = function (s, t) {
+  if (s.length > t.length) return false;
+  if (s === t) return true;
+  if (s.length > t.length) return false;
+  if (s === t) return true;
+
+  let subSeq = 0;
+  for (let i = 0; i < t.length; i++) {
+    if (s[subSeq] === t[i]) {
+      subSeq++;
+    }
+  }
+  return subSeq === s.length ? true : false;
+};
+
+console.log(isSubsequence('b', 'c'));
---
+++ ds algo/Everyday practice/75 days plan/day2Isomorphic.js
@@ -0,0 +1,22 @@
+//  leetcode 205. Isomorphic Strings
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  if (s.length !== t.length) return false;
+  if (s.length !== t.length) return false;
+  for (let i = 0; i < s.length; i++) {
+    if (
+      s.indexOf(s[i]) !== s.lastIndexOf(s[i]) ||
+      t.indexOf(t[i]) !== t.lastIndexOf(t[i])
+    ) {
+      // console.log(s[i], t[i])
+      if (t.lastIndexOf(t[i]) !== s.lastIndexOf(s[i])) return false;
+    }
+  }
+  return true;
+};
+
+console.log(isIsomorphic('egg', 'add'));
