cce5b446168a0cf87e750556396b353ff0e5f083
Rahul Kumar <rahulkumar271@gmail.com>
2022-07-29 08:11:13 +0530

    Add files via upload

---
+++ ds algo/Everyday practice/75 days plan/day3MergeTwoSortedLists.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  /**
+    Create new List, loop untill both are empty, in each loop compare the value and push the value to newList
+    */
+  let mergedList = { val: 0, next: undefined };
+  let curr = mergedList;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      curr.next = l1;
+      l1 = l1.next;
+    } else {
+      curr.next = l2;
+      l2 = l2.next;
+    }
+    curr = curr.next;
+  }
+  curr = l1 || l2;
+  return mergedList.next;
+};
---
+++ ds algo/Everyday practice/75 days plan/day3ReverseLinkedList.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+function ListNode(val, next) {
+  this.val = val ? val : 0;
+  this.next = next ? next : undefined;
+}
+
+function createNewList(num) {
+  let node = new ListNode();
+  let curr = node;
+  for (let i = 0; i < num.length; i++) {
+    curr.next = new ListNode(num[i]);
+    curr = curr.next;
+  }
+  return node.next;
+}
+
+const head = createNewList([1, 2, 3, 4, 5]);
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  /**
+     create new linkedlist, add head.next to newlist
+    */
+  let prev = null;
+  let curr = head;
+  while (curr) {
+    const temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+  }
+  return prev;
+};
+
+console.log(reverseList(head));
