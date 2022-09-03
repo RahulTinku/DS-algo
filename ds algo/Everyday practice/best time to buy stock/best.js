a604f65c1acc2c3aec803c9ba74a1efb40e55c08
Rahul Kumar <rahulkumar271@gmail.com>
2022-09-03 22:17:09 +0530

    Update best.js

--- ds algo/Everyday practice/best time to buy stock/best.js
+++ ds algo/Everyday practice/best time to buy stock/best.js
@@ -12,3 +12,21 @@ var maxProfit = function (prices) {
   }
   return result;
 };
+
+// Second method
+var maxProfit = function(prices) {
+    let min =prices[0], max = prices[0], len= prices.length, diff =0;
+    
+    for(let i=0; i< len; i++){
+      if(min > prices[i]){
+          min = prices[i];
+          max = prices[i]
+      }else{
+          if(max < prices[i]){
+              max = prices[i]
+          }
+      }
+        diff = (max -min) > diff ? max-min : diff;
+    }
+    return diff
+};
